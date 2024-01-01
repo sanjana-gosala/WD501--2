9e7488f63f65b11b1d7b971f5967edec8bef7a1c
Sanjana <21pa1a12b0@vishnu.edu.in>
2024-01-01 23:59:27 +0530

    Initial Commit

--- index.js
+++ index.js
@@ -1,4 +1,58 @@
-const hello = () =>{
-    console.log("Hello.Node.js");
-}
-hello();
+const http = require("http");
+const fs = require("fs");
+
+let homeContent = "";
+let projectContent = "";
+let registrationContent = "";
+let port = 3000; 
+
+
+process.argv.forEach((arg) => {
+    if (arg.startsWith("--port=")) {
+        const portNumber = arg.split("=")[1];
+        port = parseInt(portNumber);
+    }
+});
+
+fs.readFile("home.html", (err, home) => {
+    if (err) {
+        throw err;
+    }
+    homeContent = home;
+});
+
+fs.readFile("project.html", (err, project) => {
+    if (err) {
+        throw err;
+    }
+    projectContent = project;
+});
+
+fs.readFile("registration.html", (err, registration) => {
+    if (err) {
+        throw err;
+    }
+    registrationContent = registration;
+});
+
+http.createServer((request, response) => {
+    let url = request.url;
+    response.writeHead(200, { "Content-Type": "text/html" });
+    switch (url) {
+        case "/project":
+            response.write(projectContent);
+            response.end();
+            break;
+        case "/registration":
+            response.write(registrationContent);
+            response.end();
+            break;
+        default:
+            response.write(homeContent);
+            response.end();
+            break;
+    }
+}).listen(port, () => {
+        console.log(`Server running at http://localhost:${port}/`);
+    
+});
